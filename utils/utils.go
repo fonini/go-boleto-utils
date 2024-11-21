c73b29ebeb5ce92463b5b9e1902a6f78fab65d7c
Jonnas Fonini <jonnasfonini@gmail.com>
2024-11-21 16:36:37 -0300

    feat: Lowercase bank names and add validator

--- utils/utils.go
+++ utils/utils.go
@@ -24,3 +24,23 @@ func OnlyNumbers(str string) string {
 	re := regexp.MustCompile("[^0-9]+")
 	return re.ReplaceAllString(str, "")
 }
+
+func Mod10CheckDigit(digits string) bool {
+	var checksum int
+
+	size := len(digits)
+	for i := size - 1; i >= 0; i -= 2 {
+		n := digits[i] - '0'
+		checksum += int(n)
+	}
+	for i := size - 2; i >= 0; i -= 2 {
+		n := digits[i] - '0'
+		n *= 2
+		if n > 9 {
+			n -= 9
+		}
+		checksum += int(n)
+	}
+
+	return checksum%10 == 0
+}
---
+++ validator/validator.go
@@ -0,0 +1,35 @@
+package validator
+
+import (
+	"fmt"
+	"github.com/fonini/go-boleto-utils/parser"
+	"github.com/fonini/go-boleto-utils/utils"
+)
+
+func Validate(digitableLine string) bool {
+	boleto, err := parser.Parse(digitableLine)
+
+	if err != nil {
+		return false
+	}
+
+	var blocks []string
+
+	blocks = append(blocks, fmt.Sprintf("%s%d%s%d", boleto.IssuerBankCode, boleto.Currency, boleto.IssuerReserved1, boleto.CheckDigit1))
+	blocks = append(blocks, fmt.Sprintf("%s%d", boleto.IssuerReserved2, boleto.CheckDigit2))
+	blocks = append(blocks, fmt.Sprintf("%s%d", boleto.IssuerReserved3, boleto.CheckDigit3))
+
+	return validateBlocks(blocks)
+}
+
+func validateBlocks(blocks []string) bool {
+	var validCount = 0
+
+	for _, block := range blocks {
+		if utils.Mod10CheckDigit(block) {
+			validCount++
+		}
+	}
+
+	return validCount == len(blocks)
+}
